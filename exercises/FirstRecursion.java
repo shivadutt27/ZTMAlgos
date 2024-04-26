98e5242690dc5a3ae5f052eb27e905c3ca0396a3
shivadutt27 <devopsd07@gmail.com>
2024-04-26 08:07:37 -0700

    FirstRecurring-hashmap

--- exercises/FirstRecursion.java
+++ exercises/FirstRecursion.java
@@ -10,6 +10,13 @@ public class FirstRecursion{
         System.out.println(getFirstRecursionBrutly(new int[]{2,1,1,2,3,5,1,2,4}));
         System.out.println(getFirstRecursionBrutly(new int[]{2,3,4,5}));
 
+        System.out.println("==============HashMap=======================");
+
+        System.out.println(getFirstRecursionHashMap(new int[]{2,5,1,2,3,5,1,2,4}));
+        System.out.println(getFirstRecursionHashMap(new int[]{2,1,1,2,3,5,1,2,4}));
+        System.out.println(getFirstRecursionHashMap(new int[]{2,3,4,5}));
+
+
     }
 
     public static int getFirstRecursionBrutly(int[] arr){
@@ -18,6 +25,24 @@ public class FirstRecursion{
                 if(arr[i] == arr[j]){
                     return arr[i];
                 }
+            }
+        }
+        return 0;
+    }
+
+    /*
+     * HashMap ----> As we know know HashMap doesnot have duplicate values.
+     * By assigning each element as a key in HashMap, we know when key is being reused
+     */
+
+    public static int getFirstRecursionHashMap(int[] arr){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<arr.length; i++){
+            if(!map.containsKey(arr[i])){
+                map.put(arr[i], i);
+            }
+            else{
+                return arr[i];
             }
         }
         return 0;
